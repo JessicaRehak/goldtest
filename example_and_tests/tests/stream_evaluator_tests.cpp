13d14b132a262676d9d33ec7ef12fd667bcf0554
Joshua Rehak <jsrehak@gmail.com>
2021-05-07 10:46:50 -0700

    Initial commit of StreamEvaluatorTests.

---
+++ example_and_tests/tests/stream_evaluator_tests.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+namespace  {
+
+class StreamEvaluatorTests : public ::testing::Test {
+
+};
+
+TEST_F(StreamEvaluatorTests, Dummy) {
+  EXPECT_TRUE(false);
+}
+
+
+} // namespace
