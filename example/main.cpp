763629de3a34b8a7c68afae92cd6202e23f24d94
Joshua Rehak <jsrehak@gmail.com>
2021-05-07 09:48:53 -0700

    Added running tests to main.cpp.

--- example/main.cpp
+++ example/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+int main(int argc, char** argv) {
+  std::cout << "goldtest example program" << std::endl;
+
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
