30546cc0ada84d0706a4c48e1e6a79604c0ae8a2
Joshua Rehak <jsrehak@gmail.com>
2021-05-10 13:12:53 -0700

    Added goldtest to file writer test.

--- example_and_tests/example/tests/file_writer_test.cpp
+++ example_and_tests/example/tests/file_writer_test.cpp
@@ -1,18 +1,21 @@
 #include <gtest/gtest.h>
 
 #include "../file_writer.hpp"
+#include "../goldtest/goldtest.hpp"
 
 namespace  {
 
 class FileWriterTest : public ::testing::Test {
  public:
   using FileWriter = goldtest::example::FileWriter;
-  const std::string filename_{ "test_file.txt" };
+  const std::string filename_{ "test_file" };
 };
 
 TEST_F(FileWriterTest, ExecuteTest) {
   FileWriter test_file_writer;
-  test_file_writer.Execute(filename_);
+  test_file_writer.Execute(filename_ + ".txt");
+
+  EXPECT_GOLD_FILE_MATCH(filename_ + ".txt", "test_data/" + filename_ + ".gold");
 }
 
 } // namespace
